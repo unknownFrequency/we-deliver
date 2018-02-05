21e43ca695068cbdd72c930ff821af9d720f541b
Ruben Thuesen <rubenthuesen@unknown.local>
2018-02-05 14:35:14 +0100

    init commit. product/new form markup

---
+++ app/controllers/product_controller.rb
@@ -0,0 +1,21 @@
+class ProductController < ApplicationController
+  def index
+    
+  end
+
+  def new
+
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+end
