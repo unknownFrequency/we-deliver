d0da4494a592be587c352cee559deb00a8a4b6c6
unknownfrequency <unknownfreq@protonmail.com>
2018-02-08 12:46:47 +0100

    test to not show New Product on product_index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180208103556) do
+ActiveRecord::Schema.define(version: 20180208105940) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20180208103556) do
     t.datetime "updated_at", null: false
     t.string "category"
     t.integer "user_id"
+    t.integer "qty", default: 0, null: false
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
--- spec/features/list_products_spec.rb
+++ spec/features/list_products_spec.rb
@@ -5,14 +5,15 @@ RSpec.feature "Listing products" do
     @user = User.create!(
       name: "Ruben T", address: "her 12", zip: "7741",
       email: "a@a.a", phone: "20131262", password: "password", password_confirmation: "password")
-    login_as(@user)
 
     @product1 = Product.create(name: "Cola", description: "Bubbely", price: 25, brand: "Coca-Cola", category: "Sodavand", user: @user)
     @product2 = Product.create(name: "Colaz", description: "Bubbely", price: 25, brand: "Coca-Cola", category: "Sodavand", user: @user)
   end
 
-  scenario "We list all products" do
+  scenario "all products without user" do
     visit products_path
+    expect(page).not_to have_css("#phone")
+    expect(page).not_to have_link("Nyt Produkt")
 
     expect(page).to have_content(@product1.name)
     expect(page).to have_content(@product2.name)
@@ -26,6 +27,25 @@ RSpec.feature "Listing products" do
     expect(page).to have_content(@product2.category)
   end
 
+  scenario "all products with user" do
+    login_as(@user)
+    visit products_path
+
+    expect(page).to have_content(@user.phone)
+    expect(page).to have_css("#phone")
+
+    expect(page).to have_link("Nyt Produkt")
+    expect(page).to have_content(@product1.name)
+    expect(page).to have_content(@product2.name)
+    expect(page).to have_content(@product1.description)
+    expect(page).to have_content(@product2.description)
+    expect(page).to have_content(@product1.price)
+    expect(page).to have_content(@product2.price)
+    expect(page).to have_content(@product1.brand)
+    expect(page).to have_content(@product2.brand)
+    expect(page).to have_content(@product1.category)
+    expect(page).to have_content(@product2.category)
+  end
 
   scenario "We list all products" do
     Product.delete_all
