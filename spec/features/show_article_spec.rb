f979281e1225472c99707664620f8e98c9680f00
unknownfrequency <unknownfreq@protonmail.com>
2018-02-08 13:02:13 +0100

    only shows slet/rediger if user signed in on product_show

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -2,19 +2,18 @@ require 'rails_helper'
 
 RSpec.feature "Showing an product" do
   before do
-    @user = User.create!(
-      name: "Ruben T", address: "her 12", zip: "7741",
-      email: "a@a.a", phone: "20131262", password: "password", password_confirmation: "password")
-    login_as(@user)
+    @user1 = User.create!(email: "a@a.a", phone: "20131262", password: "password", password_confirmation: "password")
+    @user2 = User.create!(email: "b@a.a", phone: "30131269", password: "password", password_confirmation: "password")
 
     @product = Product.create(
       name: "test", brand: "test",
       description: "test", price: 100.5,
-      category: "test", user: @user
+      category: "test", user: @user1
     )
   end
   
   scenario "A user views an article" do
+    login_as(@user1)
     visit products_path
     click_link @product.name
 
@@ -23,5 +22,20 @@ RSpec.feature "Showing an product" do
     expect(page).to have_content(@product.description)
     expect(@product.price).to be_kind_of(Float)
     expect(page).to have_content(@product.category)
+    expect(page).to have_link("Rediger")
+    expect(page).to have_link("Slet")
+  end
+
+  scenario "user not signed in, views an article" do
+    visit products_path
+    click_link @product.name
+
+    expect(page).to have_content(@product.name)
+    expect(page).to have_content(@product.brand)
+    expect(page).to have_content(@product.description)
+    expect(@product.price).to be_kind_of(Float)
+    expect(page).to have_content(@product.category)
+    expect(page).not_to have_link("Rediger")
+    expect(page).not_to have_link("Slet")
   end
 end
