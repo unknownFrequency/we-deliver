7d14bdc5a39852da874ae1d425bc09fab2b86db0
Ruben Thuesen <rubenthuesen@unknown.local>
2018-02-14 10:41:29 +0100

    users add product to cart test success

--- spec/features/list_products_spec.rb
+++ spec/features/list_products_spec.rb
@@ -9,7 +9,7 @@ RSpec.feature "Listing products" do
     @brand = Brand.create(name: "test");
     @admin = User.create!(admin: 1, email: "b@a.a", phone: "77777777", password: "password", password_confirmation: "password")
     @product1 = Product.create(name: "Cola", description: "Bubbely", price: 25, brand_id: 1, user: @admin)
-    @product2 = Product.create(name: "Colaz", description: "Bubbely", price: 25, brand_id: 1, user: @admin)
+    @product2 = Product.create!(name: "Colaz", description: "Bubbely", price: 25, brand_id: 1, user: @admin)
   end
 
 
@@ -19,11 +19,26 @@ RSpec.feature "Listing products" do
     expect(page).to have_link("Nyt Produkt")
   end
 
+  scenario "user adds product to cart" do
+    login_as(@user)
+    visit products_path
+    # click_button "add-Cola"
+    click_button("add-#{@product1.name.downcase}")
+    expect(page).to have_current_path("/cart")
+    expect(page).to have_content("1 i kurv")
+
+    visit products_path
+    click_button("add-#{@product2.name.downcase}")
+    expect(page).to have_content("2 i kurv")
+    expect(page).to have_current_path("/cart")
+  end
+
   scenario "all products without user" do
     visit products_path
     expect(page).not_to have_css("#phone")
     expect(page).not_to have_link("Nyt Produkt")
 
+    page.inspect
     expect(page).to have_content(@product1.name)
     expect(page).to have_content(@product2.name)
     expect(page).to have_content(@product1.description)
